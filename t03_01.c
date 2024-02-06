c0c56f04d8a819e1a88df43a8cd3b2ce6e82d25f
clarissamanurungg <clarissamanurung0807@gmail.com>
2024-02-06 21:28:06 +0700

    049

--- t03_01.c
+++ t03_01.c
@@ -1,11 +1,29 @@
 // NIM - NAMA
 
+
 #include <stdio.h>
 
-int main(int _argc, char **_argv)
+int main(int argc, char **argv)
 {
+  int max, min, n ;
 
-  // your code here
+  scanf("%d\n", &n);
+  int angka[n];
+  min = 0;
+  max = 0;
+
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &angka[i]);
+    if (angka[i] > max) {
+      max = angka[i];
+    }
+    if (angka[i] < min) {
+      min = angka[i];
+    }
+  }
+
+  printf("%d\n", min);
+  printf("%d\n", max);
 
   return 0;
 }
